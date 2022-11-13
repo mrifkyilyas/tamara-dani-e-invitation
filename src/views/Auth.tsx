c77de0175b9c808ef661afecfc8c486ba07b118e
rifky.ilyas <rifky.ilyas@youtap.id>
2022-11-13 21:00:30 +0700

    refactor: unused import

--- src/views/Auth.tsx
+++ src/views/Auth.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import InvitationApi, { IInvitation } from '../api/invitation';
-import { NotFound } from './NotFound';
 import { useNavigate, useParams } from 'react-router-dom';
 import MessageBoxApi from '../api/message-box';
 
